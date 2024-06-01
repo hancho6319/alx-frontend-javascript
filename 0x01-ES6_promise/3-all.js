9314c1023e91b81e4ee0caac722fdbacb73c5531
hancho6319 <abbaauwalharuna@gmail.com>
2024-06-01 01:47:05 +0000

    GitHub repository: alx-frontend-javascript Directory: 0x01-ES6_promise File: 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo} ${user.firstName} ${user.lastName}`);
+      const [{ body: photo }, { firstName, lastName }] = results;
+      console.log(`${photo} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
