43be301a453af37e3c2fb1cec089ab355d590ee5
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-06-08 04:19:49 +0100

    GitHub repository: alx-frontend-javascript     Directory: 0x04-TypeScript     File: task_2/js/main.ts, task_2/webpack.config.js, task_2/tsconfig.json, task_2/package.json

---
+++ 0x04-TypeScript/task_2/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  mode: "development", // Set the mode to development
+  entry: "./js/main.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    open: true,
+    hot: true
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  performance: {
+    hints: "warning", // Show performance hints
+    maxAssetSize: 244 * 1024, // 244 KiB
+    maxEntrypointSize: 244 * 1024 // 244 KiB
+  }
+};
+
